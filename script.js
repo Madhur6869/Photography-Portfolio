4da0117f26c677a918620573717c315ebc7c9ba5
Madhur <madhur.padavala2002@gmail.com>
2020-11-19 23:20:18 +0530

    loader fix

--- script.js
+++ script.js
@@ -11,6 +11,20 @@
     } 
 })
 
+/*document.querySelector("body").style.visibility = "hidden"; 
+document.querySelector(".loader").style.visibility = "visible";
+console.log("Loading"); 
+
+$(window).load(function() {
+    // executes when complete page is fully loaded, including all frames, objects and images
+    document.querySelector(".loader").style.display = "none"; 
+    document.querySelector(".loader").style.visibility = "hidden";
+    document.querySelector("body").style.visibility = "visible";
+   
+    console.log("Loaded"); 
+   });
+   
+*/
 function redirect(name) {
     console.log(name);
     var url = "Snippets/"+ name +"/"+ name +".html";
