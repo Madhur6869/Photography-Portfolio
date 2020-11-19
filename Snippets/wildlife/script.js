b63f0c26ded5f285a717698f8675d0d6d4940e92
Madhur <madhur.padavala2002@gmail.com>
2020-11-19 15:56:05 +0530

    Added loader

--- Snippets/wildlife/script.js
+++ Snippets/wildlife/script.js
@@ -1,11 +1,12 @@
-/*document.onreadystatechange = function() { 
+(document.onreadystatechange = function() { 
     if (document.readyState !== "complete") { 
         document.querySelector("body").style.visibility = "hidden"; 
-        document.querySelector(".loader-wrapper").style.visibility = "visible";
+        document.querySelector(".loader").style.visibility = "visible";
         console.log("Loading"); 
     } else { 
-        document.querySelector(".loader-wrapper").style.display = "none"; 
+        document.querySelector(".loader").style.display = "none"; 
         document.querySelector("body").style.visibility = "visible";
+        document.querySelector(".loader").style.visibility = "hidden";
         console.log("Loaded"); 
     } 
-}; */
+}) 
--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-/*document.onreadystatechange = function() { 
+(document.onreadystatechange = function() { 
     if (document.readyState !== "complete") { 
         document.querySelector("body").style.visibility = "hidden"; 
-        document.querySelector(".loader-wrapper").style.visibility = "visible";
+        document.querySelector(".loader").style.visibility = "visible";
         console.log("Loading"); 
     } else { 
-        document.querySelector(".loader-wrapper").style.display = "none"; 
+        document.querySelector(".loader").style.display = "none"; 
         document.querySelector("body").style.visibility = "visible";
+        document.querySelector(".loader").style.visibility = "hidden";
         console.log("Loaded"); 
     } 
-}; */
+}) 
+
 function redirect(name) {
     console.log(name);
     var url = "Snippets/"+ name +"/"+ name +".html";
