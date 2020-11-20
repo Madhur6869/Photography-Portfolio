7bef42900679634b86317b2e301a78e63c0e8a7d
Madhur <madhur.padavala2002@gmail.com>
2020-11-20 16:23:18 +0530

    fixed loader page glitch

--- script.js
+++ script.js
@@ -1,30 +1,8 @@
-(document.onreadystatechange = function() { 
-    if (document.readyState !== "complete") { 
-        document.querySelector("body").style.visibility = "hidden"; 
-        document.querySelector(".loader").style.visibility = "visible";
-        console.log("Loading"); 
-    } else { 
-       // document.querySelector(".loader").style.display = "none"; 
-        document.querySelector("body").style.visibility = "visible";
-        document.querySelector(".loader").style.visibility = "hidden";
-        console.log("Loaded"); 
-    } 
-})
-
-/*document.querySelector("body").style.visibility = "hidden"; 
-document.querySelector(".loader").style.visibility = "visible";
-console.log("Loading"); 
-
-$(window).load(function() {
-    // executes when complete page is fully loaded, including all frames, objects and images
-    document.querySelector(".loader").style.display = "none"; 
-    document.querySelector(".loader").style.visibility = "hidden";
-    document.querySelector("body").style.visibility = "visible";
-   
-    console.log("Loaded"); 
-   });
-   
-*/
+window.addEventListener('load', (event) => {
+    $("div").remove(".loader");
+    console.log('page is fully loaded');
+  });
+ 
 function redirect(name) {
     console.log(name);
     var url = "Snippets/"+ name +"/"+ name +".html";
